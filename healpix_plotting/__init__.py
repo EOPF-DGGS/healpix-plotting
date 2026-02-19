cba1d8692c8ec7da957213b28a3481c1707c96c7
Justus Magin <keewis@posteo.de>
2026-02-19 17:50:33 +0100

    expose `EllipsoidLike`

--- healpix_plotting/__init__.py
+++ healpix_plotting/__init__.py
@@ -5,9 +5,10 @@ try:
 except Exception:
     __version__ = "9999"
 
+from healpix_plotting.ellipsoid import EllipsoidLike
 from healpix_plotting.healpix import HealpixGrid
 from healpix_plotting.plotting import plot
 from healpix_plotting.resample import resample
 from healpix_plotting.sampling_grid import SamplingGrid
 
-__all__ = ["HealpixGrid", "plot", "resample", "SamplingGrid"]
+__all__ = ["HealpixGrid", "plot", "resample", "SamplingGrid", "EllipsoidLike"]
