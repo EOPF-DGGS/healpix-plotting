8e00adce7778500a45a38afd06e48369ac2cc1dc
Justus Magin <keewis@posteo.de>
2026-02-19 16:19:09 +0100

    export the most important functions and classes

--- healpix_plotting/__init__.py
+++ healpix_plotting/__init__.py
@@ -4,3 +4,10 @@ try:
     __version__ = version("healpix_plotting")
 except Exception:
     __version__ = "9999"
+
+from healpix_plotting.healpix import HealpixGrid
+from healpix_plotting.plotting import plot
+from healpix_plotting.resample import resample
+from healpix_plotting.sampling_grid import SamplingGrid
+
+__all__ = ["HealpixGrid", "plot", "resample", "SamplingGrid"]
